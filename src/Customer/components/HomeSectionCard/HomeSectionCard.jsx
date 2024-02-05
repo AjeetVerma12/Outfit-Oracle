e173f9f9466cfec2718b6fcfc6ccd50b2b8dfef8
AjeetVerma2912 <ajeet120300@gmail.com>
2024-02-06 01:41:19 +0530

    carousel functioning

--- src/Customer/components/HomeSectionCard/HomeSectionCard.jsx
+++ src/Customer/components/HomeSectionCard/HomeSectionCard.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
-const HomeSectionCard = () => {
+const HomeSectionCard = ({ product }) => {
   return (
     <div className="cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-lg overflow-hidden w-[15rem] mx-3 border">
       <div className="h-[13rem] w-[10rem]">
         <img
           className="object-cover object-top w-full h-full"
-          src="https://jompers.com/cdn/shop/products/KO_640Olive_2.jpg?crop=center&height=600&v=1676715597&width=400"
+          src={product.imageUrl}
           alt=""
         ></img>
       </div>
       <div className="p-4">
-        <h3 className="text-lg font-medium text-grey-900">Jompers</h3>
-        <p className="mt-2 text-sm text-grey-500">MEN'S EMBROIDERD KURTAS</p>
+        <h3 className="text-lg font-medium text-grey-900">{product.brand}</h3>
+        <p className="mt-2 text-sm text-grey-500">{product.title}</p>
       </div>
     </div>
   );
--- src/Customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ src/Customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -1,30 +1,56 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import AliceCarousel from "react-alice-carousel";
 import HomeSectionCard from "../HomeSectionCard/HomeSectionCard";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
 import { Button } from "@mui/material";
 
-const HomeSectionCarousel = () => {
+const HomeSectionCarousel = ({data, sectionName}) => {
+    const [activeIndex, setActiveIndex]=useState(0);
+    const carouselRef = useRef(null);
+
   const responsive = {
     0: { items: 1 },
     720: { items: 3 },
     1024: { items: 5.5 },
   };
 
-  const items = [1, 1, 1, 1, 1].map((item) => <HomeSectionCard />);
+//   const slidePrev=()=>setActiveIndex(activeIndex-1);
+//   const slideNext=()=>setActiveIndex(activeIndex+1)
+
+//   const syncActiveIndex=({item})=>setActiveIndex(item)
+
+const slidePrev = () => {
+    if (carouselRef.current) {
+      carouselRef.current.slidePrev();
+    }
+  };
+
+  const slideNext = () => {
+    if (carouselRef.current) {
+      carouselRef.current.slideNext();
+    }
+  };
+
+  const syncActiveIndex = ({ item }) => setActiveIndex(item);
+
+  const items = data.slice(0,10).map((item) => (<HomeSectionCard key={item.id} product={item} />));
   return (
     <div className=" border">
+        <h2 className="text-2xl font-extrabold text-gray-800 py-5">{sectionName}</h2>
       <div className="relative p-5">
         <AliceCarousel
           items={items}
           disableButtonsControls
-          infinite
           responsive={responsive}
           disableDotsControls
+          onSlideChanged={syncActiveIndex}
+          activeIndex={activeIndex}
+          ref={carouselRef}
         />
-        <Button
+        {activeIndex !== items.length-5 && (<Button
           variant="contained"
-          className="z-50 "
+          className="z-50 bg-white"
+          onClick={slideNext}
           sx={{
             position: "absolute",
             top: "8rem",
@@ -37,11 +63,12 @@ const HomeSectionCarousel = () => {
           <KeyboardArrowLeftIcon
             sx={{ transform: "rotate(90deg)", color: "black" }}
           />
-        </Button>
+        </Button> )}
 
-        <Button
-          variant="contained"
-          className="z-50 "
+        {activeIndex !== 0 && (<Button
+        onClick={slidePrev}
+        variant="contained"
+          className="z-50 bg-white"  
           sx={{
             position: "absolute",
             top: "8rem",
@@ -54,7 +81,7 @@ const HomeSectionCarousel = () => {
           <KeyboardArrowLeftIcon
             sx={{ transform: "rotate(-90deg)", color: "black" }}
           />
-        </Button>
+        </Button> )}
       </div>
     </div>
   );
